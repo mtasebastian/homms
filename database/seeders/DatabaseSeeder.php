d63ea2d69691a6b7a581dd6c4a65866991b2f508
Ericson <ericsonpaciente23@gmail.com>
2025-11-09 18:12:48 +0800

    Test fix for migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            AddressSeeder::class,
+            ModuleSeeder::class,
+            PermissionsSeeder::class,
+            RolesSeeder::class
+        ]);
     }
 }
